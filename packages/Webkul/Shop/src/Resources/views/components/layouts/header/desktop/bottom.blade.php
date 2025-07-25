960b7b196fd5321b6f1a0ffb8913b95c29ce127d
Hassaan Ali <zfhassaan@gmail.com>
2025-07-25 09:53:25 +0500

    zzxm11.0

--- packages/Webkul/Shop/src/Resources/views/components/layouts/header/desktop/bottom.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/layouts/header/desktop/bottom.blade.php
@@ -14,7 +14,7 @@
             aria-label="@lang('shop::app.components.layouts.header.desktop.bottom.bagisto')"
         >
             <img
-                src="{{ core()->getCurrentChannel()->logo_url ?? asset('storage/configuration/logo.png')  }}"
+                src="{{ core()->getCurrentChannel()->logo_url ?? bagisto_asset('images/logo.svg')  }}"
                 width="131"
                 height="29"
                 alt="{{ config('app.name') }}"
